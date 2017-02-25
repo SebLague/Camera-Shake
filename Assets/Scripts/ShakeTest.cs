0e24c902cd4841c0c92e4ee7991d12cf923d8c4c
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-02-25 08:32:37 +0200

    Added tutorial files

---
+++ Assets/Scripts/ShakeTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTest : MonoBehaviour {
+
+	public CameraShake.Properties testProperties;
+
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			FindObjectOfType<CameraShake>().StartShake (testProperties);
+		}
+	}
+}
